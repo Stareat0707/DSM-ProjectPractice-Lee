e87352dc0a9dc5823054263a199e5676a022672f
Stareat0707 <stareat0707@gmail.com>
2018-04-17 10:31:01 +0900

    Add FeetToMeter and MeterToFeet

--- DistanceConverter/Program.cs
+++ DistanceConverter/Program.cs
@@ -10,6 +10,29 @@ namespace DistanceConverter
     {
         static void Main(string[] args)
         {
+            for (int feet = 1; feet <= 10; ++feet)
+            {
+                double meter = FeetToMeter(feet);
+                Console.WriteLine("{0}ft = {1:0.0000}m", feet, meter);
+            }
+
+            Console.WriteLine();
+
+            for (int meter = 1; meter <= 10; ++meter)
+            {
+                double feet = MeterToFeet(meter);
+                Console.WriteLine("{0}m = {1:0.0000}ft", meter, feet);
+            }
+        }
+
+        static double FeetToMeter(double feet)
+        {
+            return feet * 0.3048;
+        }
+
+        static double MeterToFeet(double meter)
+        {
+            return meter / 0.3048;
         }
     }
 }
