325e57e502d94bad91069582113be4055f4cfe4a
Stareat0707 <stareat0707@gmail.com>
2018-06-12 08:54:45 +0900

    To do list 작성

---
+++ 01_08_나성식_TodoList/Form1.Designer.cs
@@ -0,0 +1,191 @@
+﻿namespace ToDoList
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.Work = new System.Windows.Forms.TextBox();
+            this.Date = new System.Windows.Forms.TextBox();
+            this.Description = new System.Windows.Forms.TextBox();
+            this.Important = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.Open = new System.Windows.Forms.Button();
+            this.Save = new System.Windows.Forms.Button();
+            this.Exit = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("나눔고딕", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(65, 28);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "할 일";
+            // 
+            // Work
+            // 
+            this.Work.Font = new System.Drawing.Font("나눔고딕", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.Work.Location = new System.Drawing.Point(165, 6);
+            this.Work.Name = "Work";
+            this.Work.Size = new System.Drawing.Size(270, 35);
+            this.Work.TabIndex = 1;
+            // 
+            // Date
+            // 
+            this.Date.Font = new System.Drawing.Font("나눔고딕", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.Date.Location = new System.Drawing.Point(165, 47);
+            this.Date.Name = "Date";
+            this.Date.Size = new System.Drawing.Size(270, 35);
+            this.Date.TabIndex = 2;
+            // 
+            // Description
+            // 
+            this.Description.Font = new System.Drawing.Font("나눔고딕", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.Description.Location = new System.Drawing.Point(165, 129);
+            this.Description.Multiline = true;
+            this.Description.Name = "Description";
+            this.Description.Size = new System.Drawing.Size(270, 154);
+            this.Description.TabIndex = 3;
+            // 
+            // Important
+            // 
+            this.Important.Font = new System.Drawing.Font("나눔고딕", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.Important.Location = new System.Drawing.Point(165, 88);
+            this.Important.Name = "Important";
+            this.Important.Size = new System.Drawing.Size(270, 35);
+            this.Important.TabIndex = 0;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("나눔고딕", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.label2.Location = new System.Drawing.Point(12, 50);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(58, 28);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "기한";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("나눔고딕", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.label3.Location = new System.Drawing.Point(12, 91);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(81, 28);
+            this.label3.TabIndex = 0;
+            this.label3.Text = "중요도";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("나눔고딕", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.label4.Location = new System.Drawing.Point(12, 132);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(104, 28);
+            this.label4.TabIndex = 0;
+            this.label4.Text = "상세설명";
+            // 
+            // Open
+            // 
+            this.Open.AutoSize = true;
+            this.Open.Font = new System.Drawing.Font("나눔고딕", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.Open.Location = new System.Drawing.Point(202, 289);
+            this.Open.Name = "Open";
+            this.Open.Size = new System.Drawing.Size(75, 38);
+            this.Open.TabIndex = 4;
+            this.Open.Text = "열기";
+            this.Open.UseVisualStyleBackColor = true;
+            this.Open.Click += new System.EventHandler(this.Open_Click);
+            // 
+            // Save
+            // 
+            this.Save.AutoSize = true;
+            this.Save.Font = new System.Drawing.Font("나눔고딕", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.Save.Location = new System.Drawing.Point(283, 289);
+            this.Save.Name = "Save";
+            this.Save.Size = new System.Drawing.Size(75, 38);
+            this.Save.TabIndex = 5;
+            this.Save.Text = "저장";
+            this.Save.UseVisualStyleBackColor = true;
+            this.Save.Click += new System.EventHandler(this.Save_Click);
+            // 
+            // Exit
+            // 
+            this.Exit.AutoSize = true;
+            this.Exit.Font = new System.Drawing.Font("나눔고딕", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.Exit.Location = new System.Drawing.Point(364, 289);
+            this.Exit.Name = "Exit";
+            this.Exit.Size = new System.Drawing.Size(75, 38);
+            this.Exit.TabIndex = 0;
+            this.Exit.Text = "닫기";
+            this.Exit.UseVisualStyleBackColor = true;
+            this.Exit.Click += new System.EventHandler(this.Exit_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(447, 337);
+            this.Controls.Add(this.Exit);
+            this.Controls.Add(this.Save);
+            this.Controls.Add(this.Open);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.Important);
+            this.Controls.Add(this.Description);
+            this.Controls.Add(this.Date);
+            this.Controls.Add(this.Work);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "ToDo List 작성 앱";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox Work;
+        private System.Windows.Forms.TextBox Date;
+        private System.Windows.Forms.TextBox Description;
+        private System.Windows.Forms.TextBox Important;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button Open;
+        private System.Windows.Forms.Button Save;
+        private System.Windows.Forms.Button Exit;
+    }
+}
+
---
+++ 01_08_나성식_TodoList/Form1.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ToDoList
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Open_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.Filter = "텍스트 파일|*.txt";
+            openFileDialog.Title = "To Do List 열기";
+            openFileDialog.ShowDialog();
+
+            if (openFileDialog.FileName != "")
+            {
+                FileStream fs = (FileStream)openFileDialog.OpenFile();
+                StreamReader streamReader = new StreamReader(fs);
+                Work.Text = streamReader.ReadLine();
+                Date.Text = streamReader.ReadLine();
+                Important.Text = streamReader.ReadLine();
+                Description.Text = streamReader.ReadLine();
+                streamReader.Close();
+                fs.Close();
+            }
+        }
+
+        private void Save_Click(object sender, EventArgs e)
+        {
+            SaveFileDialog saveFileDialog = new SaveFileDialog();
+            saveFileDialog.Filter = "텍스트 파일|*.txt";
+            saveFileDialog.Title = "To Do List 저장";
+            saveFileDialog.ShowDialog();
+
+            if (saveFileDialog.FileName != "")
+            {
+                FileStream fs = (FileStream)saveFileDialog.OpenFile();
+                StreamWriter streamWriter = new StreamWriter(fs);
+                streamWriter.WriteLine(Work.Text);
+                streamWriter.WriteLine(Date.Text);
+                streamWriter.WriteLine(Important.Text);
+                streamWriter.WriteLine(Description.Text);
+                streamWriter.Close();
+                fs.Close();
+
+                Work.Text = "";
+                Date.Text = "";
+                Important.Text = "";
+                Description.Text = "";
+            }
+        }
+
+        private void Exit_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+    }
+}
